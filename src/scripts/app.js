6cafe793f533583f359e043a1bc05f050e354c7e
Josh <notjoshmiller@gmail.com>
2014-05-23 13:00:49 -0500

    Add pagination. Working so far.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,7 @@
     'use strict';
 
     var chats = {
+        chatID: m.prop(''),
         list: m.prop([]),
         transcript: m.prop([]),
         accountType: m.prop('ddi'),
@@ -11,6 +12,7 @@
                 return m.request({method: 'GET', url: url})
         },
         controller: function() {
+            this.chatID = chats.chatID;
             this.list = chats.list;
             this.accountType = chats.accountType;
             this.account = chats.currentAccount;
@@ -18,6 +20,47 @@
             this.trans = chats.transcript;
             this.sized = false;
             this.extended = false;
+            this.rowsPerPage = 6;
+            this.currentPage = 1;
+            this.startPage = 0;
+            this.endPage = 5;
+
+            this.paginated = function() {
+                return this.list().slice((this.currentPage-1) * this.rowsPerPage,
+                    ((this.currentPage-1)*this.rowsPerPage) + this.rowsPerPage)
+            }.bind(this);
+
+            this.numPages = function() {
+                var numPages = 0;
+                if (this.list() !== null && this.rowsPerPage !== null) {
+                    numPages = Math.ceil(this.list().length / this.rowsPerPage);
+                }
+                return range(1, numPages+1);
+            }.bind(this);
+
+            this.showPage = function(e) {
+                this.currentPage = e.target.innerHTML;
+                clearSelected();
+                tableRender(this);
+                controlsRender(this);
+                if (this.chatID()) makeSelected('.btn[value='+this.chatID()+']');
+            }.bind(this);
+
+            this.nextPage = function() {
+                if (this.endPage !== this.numPages) {
+                    ++this.startPage;
+                    ++this.endPage;
+                    controlsRender(this);
+                }
+            }.bind(this);
+
+            this.prevPage = function() {
+                if (this.startPage > 0) {
+                    --this.startPage;
+                    --this.endPage;
+                    controlsRender(this);
+                }
+            }.bind(this);
 
             this.search = function(e) {
                 e.preventDefault();
@@ -29,11 +72,13 @@
                     chats.get(url)
                         .then(this.list)
                         .then(function() { return tableRender(this)}.bind(this))
+                        .then(function() { return controlsRender(this)}.bind(this))
                 }
             }.bind(this);
 
             this.transcript = function(e) {
                 var url = 'api/transcripts/' + e.target.value;
+                this.chatID(e.target.value);
                 this.lastAccount(this.account());
                 clearSelected();
                 makeSelected(e.target);
@@ -97,7 +142,8 @@
                                 }, [ 'E&A', m('input[type=radio].btn.btn-default.btn-sm')
                             ]),
                         ]),
-                        m('div#table')
+                        m('div#table'),
+                        m('div#controls')
                     ])
                 ]),
                 m('div#transcript-controls', [
@@ -119,7 +165,7 @@
                     m('th', 'Racker')
                 ])
             ]),
-            m('tbody', [ ctrl.list().map(function(chat, index) {
+            m('tbody', [ ctrl.paginated().map(function(chat, index) {
                 return m('tr', [
                     m('td', [
                         m('button[type=button].btn.btn-sm', {
@@ -137,6 +183,25 @@
 
     var tableRender = function(ctrl) {
         m.render(document.getElementById('table'), table(ctrl));
+    }
+
+    var controls = function(ctrl) {
+        return m('ul.list-inline.text-center', [
+            ctrl.startPage > 0 ? m('li', [ m('a.btn.btn-default.btn-sm', {onclick: ctrl.prevPage}, '<<' )]) : null,
+            ctrl.numPages().slice(ctrl.startPage, ctrl.endPage).map(function(page, index) {
+                return m('li', [
+                    m('a', {
+                        onclick: ctrl.showPage,
+                        class: ctrl.currentPage == page ? 'btn btn-default btn-sm active' : 'btn btn-default btn-sm'},
+                    page)
+                ])
+            }),
+            ctrl.numPages().length > ctrl.endPage ? m('li', [ m('a.btn.btn-default.btn-sm', {onclick: ctrl.nextPage}, '>>')]) : null
+        ])
+    }
+
+    var controlsRender = function(ctrl) {
+        m.render(document.getElementById('controls'), controls(ctrl));
     }
 
     var transcript = function(ctrl) {
@@ -206,6 +271,25 @@
     var clearSelected = function() {
         $('.selected').removeClass('selected');
         $('.selected-button').removeClass('selected-button');
+    }
+
+    var range = function(start, stop, step) {
+        if (arguments.length <= 1) {
+              stop = start || 0;
+              start = 0;
+            }
+            step = arguments[2] || 1;
+
+            var length = Math.max(Math.ceil((stop - start) / step), 0);
+            var idx = 0;
+            var range = new Array(length);
+
+            while(idx < length) {
+              range[idx++] = start;
+              start += step;
+            }
+
+            return range;
     }
 
     m.route.mode = 'pathname';
